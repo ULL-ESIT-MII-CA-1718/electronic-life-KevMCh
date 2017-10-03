9a76f05cc579ab5b51b78dd3e0a137c6d2d12393
Kevin <marchinkev@gmail.com>
2017-10-03 11:33:15 +0100

    Added static server to the gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,14 @@ var shell = require('gulp-shell');
 gulp.task("default", ["test"]);
 
 gulp.task("preinstall", shell.task([
+      "npm install -g static-server",
       "npm install -g phantomjs",
       "npm install -g jshint",
       "npm install -g html-lint",
       "npm install -g csslint"
 ]));
+
+gulp.task("serve", shell.task("static-server -p 8080"));
 
 gulp.task("test", shell.task("./node_modules/mocha/bin/mocha --require should"));
 
